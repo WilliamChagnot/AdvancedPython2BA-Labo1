73905e02fd22eaf8c35079d621d8ba79c895942e
William Chagnot <14151@ecam.be>
2016-02-03 14:49:13 +0100

    add config files

---
+++ test_utils.py
@@ -0,0 +1,26 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 2, 2016
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+        if n<0:
+            raise ValueError
+        pass
+    
+    def test_roots(self):
+        
+        pass
+    
+    def test_integrate(self):
+        if upper < lower:
+            raise ValueError
+        pass
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
